03dc5b6ee6a4257b26140fcb4766d0c79b0f487d
Spencer Lindemuth <lindemut@gmail.com>
2019-03-27 18:05:44 +0000

    Automatically backed up by Learn

--- json.rb
+++ json.rb
@@ -5,5 +5,5 @@ url = 'https://data.cityofnewyork.us/resource/7btz-mnc8.json'
 response = HTTParty.get(url)
 list = response.parsed_response
 list.each do |x|
-  puts "#{x["provider_first_name"]} #{x["provider_last_name"]}""
+  puts "#{x["provider_first_name"]} #{x["provider_last_name"]}"
 end
