70b32c1cacac0b985cc35217fa8db149ecb20092
Spencer Lindemuth <lindemut@gmail.com>
2019-03-27 18:06:43 +0000

    Automatically backed up by Learn

--- json.rb
+++ json.rb
@@ -5,5 +5,7 @@ url = 'https://data.cityofnewyork.us/resource/7btz-mnc8.json'
 response = HTTParty.get(url)
 list = response.parsed_response
 list.each do |x|
-  puts "#{x["provider_first_name"]} #{x["provider_last_name"]}"
+  if x["practice_borough"] == "Staten Island" 
+    puts "#{x["provider_first_name"]} #{x["provider_last_name"]}"
+  end
 end
